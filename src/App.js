51230f0489cbc44cad699a4f390311bf32e2ac99
unknown <alieldamiry@gmail.com>
2021-07-14 07:41:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import { Switch, Route } from "react-router-dom";
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Dashboard /> */}
+      <Layout>
+        <Switch>
+          <Route path="/orders">
+            <div>Orders</div>
+          </Route>
+          <Route path="/customers">
+            <div>customers</div>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
---
+++ src/components/Copyrights.js
@@ -0,0 +1,19 @@
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+  
+  const Copyrights = () => {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    )
+  }
+  
+  export default Copyrights
+  
+
